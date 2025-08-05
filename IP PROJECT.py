032fcb398abb9f7577e814147de6b94d94680067
StellaHelin <stellahelin41@gmail.com>
2025-08-05 10:19:04 +0530

    Update IP PROJECT.py
    Added more functions

--- IP PROJECT.py
+++ IP PROJECT.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def home_page():
     while True:
         print("""
@@ -25,16 +28,17 @@ def home_page():
         print("______________________________________________________________________________________________________________________________________________________")
         choice=int(input("Enter your choice : "))
         if choice==1:
-            print("""
+            print(r"""
                                                   ____              _              _____ _      _        _   
                                                  | __ )  ___   ___ | | __   __ _  |_   _(_) ___| | _____| |_ 
                                                  |  _ \ / _ \ / _ \| |/ /  / _` |   | | | |/ __| |/ / _ \ __|
                                                  | |_) | (_) | (_) |   <  | (_| |   | | | | (__|   <  __/ |_ 
                                                  |____/ \___/ \___/|_|\_\  \__,_|   |_| |_|\___|_|\_\___|\__|""")
                                                                  
+
             page_book()
         elif choice==2:
-            print("""  
+            print(r"""  
                                                    __  __         ____              _    _             
                                                   |  \/  |_   _  | __ )  ___   ___ | | _(_)_ __   __ _ 
                                                   | |\/| | | | | |  _ \ / _ \ / _ \| |/ / | '_ \ / _` |
@@ -43,7 +47,7 @@ def home_page():
                                                            |___/                                  |___/ """)
             page_booked()
         elif choice==3:
-            print("""                             ____                      _  _____ _      _        _   
+            print(r"""                             ____                      _  _____ _      _        _   
                                                  / ___|__ _ _ __   ___ ___ | ||_   _(_) ___| | _____| |_ 
                                                 | |   / _` | '_ \ / __/ _ \  |  | | | |/ __| |/ / _ \ __|
                                                 | |__| (_| | | | | (_|  __/  |  | | | | (__|   <  __/ |_ 
@@ -59,17 +63,42 @@ def home_page():
 
 
 """)
-          page_about()
-      
-        elif choice==5:
-          print("""   ____            _                   
-                     |  _ \ _____   _(_) _____      _____ 
-                     | |_) / _ \ \ / / |/ _ \ \ /\ / / __|
-                     |  _ <  __/\ V /| |  __/\ V  V /\__ \
-                     |_| \_\___| \_/ |_|\___| \_/\_/ |___/""")
-                                      
+                                                               
         
-              
+            page_about()
+        elif choice==5:
+            print(r"""
+                                                              ____            _                   
+                                                             |  _ \ _____   _(_) _____      _____ 
+                                                             | |_) / _ \ \ / / |/ _ \ \ /\ / / __|
+                                                             |  _ <  __/\ V /| |  __/\ V  V /\__ \
+                                                             |_| \_\___| \_/ |_|\___| \_/\_/ |___/""")
+
+            reviews()
+        elif choice==6:
+             print(r"""
+                                                                      _   _      _       
+                                                                     | | | | ___| |_ __  
+                                                                     | |_| |/ _ \ | '_ \ 
+                                                                     |  _  |  __/ | |_) |
+                                                                     |_| |_|\___|_| .__/ 
+                                                                                  |_|    """)
+             page_help()
+        elif choice==7:
+            print(r"""
+                                                                      _____      _ _   
+                                                                     | ____|_  _(_) |_ 
+                                                                     |  _| \ \/ / | __|
+                                                                     | |___ >  <| | |_ 
+                                                                     |_____/_/\_\_|\__|""")
+            page_exit()
+            break
+    
+        else:
+            print("Invalid choice")
+                   
+
+
 def page_about():
     print("""Welcome to BlueBus, your trusted companion for seamless and affordable bus travel.
 
@@ -94,4 +123,67 @@ This project is a part of our learning journey in software development — combi
 We believe smart travel should be for everyone — and BlueBus is our step in that direction.""")
     print()
     input("Please press enter to go back")
+
+
+def reviews():
+    while True:
+        print("""
+    1)Give a review
+    2)Read reviews
+    3)Return to homepage
+    """)
+        print("______________________________________________________________________________________________________________________________________________________")
+        choice2=int(input("Enter your choice : "))
+        if choice2==1:
+            give_review()
+        elif choice2==2:
+            read_review()
+        elif choice2==3:
+            home_page()
+        else:
+            print("Invalid choice")
+            reviews()
+def give_review():
+    name=input("Enter yout name : ")
+    rate=int(input("Rate your experience (1-5): "))
+    if 1<=rate<=5:
+        rate="⭐"*rate
+    else:
+        print("Please enter a rating between 1 and 5.")
+        return give_review()
+    description=input("Enter your review :")
+    print("Rating : ",rate)
+    print("Name : ",name)
+    print("Review : ",description)
+    print()
+    confirm(rate,name,description)
+def confirm(rate,name,description):
+        choice3=input("Confirm details ? Y/N : ")
+        if choice3=="Y":
+            d={"Rating":[rate],"Name":[name],"Review":[description]}
+            df=pd.DataFrame(d)
+            df.to_csv(r"C:\Users\stell\OneDrive\Desktop\IP\review.csv",header=False,mode="a",index=False)
+            print()
+            print("Thanks for sharing your thoughts with BlueBus! Safe travels!")
+            print()
+            input("Press enter to back")
+            print()
+            home_page()
+        elif choice3=="N":
+            return give_review()
+        else:
+            print("Invalid choice")
+            print()
+            return confirm(rate,name,description)
+                       
+    
+def page_exit():
+    print("""
+             __   __                _                                          _      _                    __                           _  
+             \ \ / /__ _  _ _ _    (_)___ _  _ _ _ _ _  ___ _  _   ___ _ _  __| |___ | |_  ___ _ _ ___    / _|___ _ _   _ _  _____ __ _| | 
+              \ V / _ \ || | '_|   | / _ \ || | '_| ' \/ -_) || | / -_) ' \/ _` (_-< | ' \/ -_) '_/ -_)  |  _/ _ \ '_| | ' \/ _ \ V  V /_| 
+               |_|\___/\_,_|_|    _/ \___/\_,_|_| |_||_\___|\_, | \___|_||_\__,_/__/ |_||_\___|_| \___|  |_| \___/_|   |_||_\___/\_/\_/(_)
+                                 |__/                       |__/                                 """)
 home_page()
+print("hi")
+
